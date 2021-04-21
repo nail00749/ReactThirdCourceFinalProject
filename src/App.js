bb5e6bb0e4085e40435376d8b92c557d12fa07e8
nail00749 <71979155+nail00749@users.noreply.github.com>
2021-04-21 19:41:46 +0500

    Biba and Boba

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, {Component} from "react";
 import './App.css';
+import MainPanel from "./Components/MainPanel";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    state = {
+        urlFilters: "https://api.jsonbin.io/b/5d4beaabf090a433857b567a/latest",
+        urlCards: "https://api.jsonbin.io/b/5d4be91d00947c04a5a75da2/latest",
+        dataFilter: null,
+
+    }
+
+    Delay(ms) {
+        return new Promise(resolve => setTimeout(() => {
+        }, ms))
+    }
+
+    GetDataFilters = () => {
+        console.log("StartGetDataFilters...")
+        fetch(this.state.urlFilters).then((response) => {
+            return response.json();
+        })
+            .then((data) => {
+                console.log(data);
+                this.setState({dataFilter: data})
+                console.log(this.state.dataFilter);
+            });
+    }
+
+
+    GetData = () => {
+        console.log(this.state.dataFilter.videos)
+        MainPanel.bind(this.state.dataFilter.videos)
+    }
+
+    render() {
+        return (
+            <div>
+                <MainPanel data={this.state.dataFilter}></MainPanel>
+                <button onClick={this.GetDataFilters}>GetFilters</button>
+                <button onClick={this.GetData}>ConsoleLogData</button>
+            </div>
+        );
+    }
 }
+
 
 export default App;
