d0cd318bf5522855510175fed1a83e67691b7b38
wifelly <supermegawifi@gmail.com>
2021-04-21 21:41:16 +0500

    Верстка меню карточек

--- src/Components/CardsMenu.js
+++ src/Components/CardsMenu.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./CardsMenu.css"
 
 class CardsMenu extends React.Component {
 
@@ -9,10 +10,18 @@ class CardsMenu extends React.Component {
     render() {
         if (this.props.dataCards) {
             let listItems = this.props.dataCards.map((dataCards) =>
-                <div>{dataCards.text}</div>
+                <div key={dataCards.id} className="Card">
+                    <div className="CardImage">
+                        <img src='https://smoking-shop.ru/upload/catalog_files/ce4/ce49cfe5904c8982d9aba7a61d699d4a.jpg'
+                             alt='hmmm'/>
+                    </div>
+                    <div className="container">
+                        {dataCards.text}
+                    </div>
+                </div>
             )
             return (
-                <div>{listItems}</div>
+                <div className='CardsMenu'>{listItems}</div>
             );
         } else {
             return <div></div>
