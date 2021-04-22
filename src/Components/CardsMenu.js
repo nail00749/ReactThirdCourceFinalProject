3b974d6c917834a69bca64a1a4c52d7aef1919d4
wifelly <supermegawifi@gmail.com>
2021-04-22 21:29:24 +0500

    Верстка меню карточек

--- src/Components/CardsMenu.js
+++ src/Components/CardsMenu.js
@@ -15,8 +15,9 @@ class CardsMenu extends React.Component {
                         <img src='https://smoking-shop.ru/upload/catalog_files/ce4/ce49cfe5904c8982d9aba7a61d699d4a.jpg'
                              alt='hmmm'/>
                     </div>
-                    <div className="container">
-                        {dataCards.text}
+                    <div className="Container">
+                        <h3>{dataCards.category}</h3>
+                        <p>{dataCards.text}</p>
                     </div>
                 </div>
             )
