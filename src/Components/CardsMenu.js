64570ae13e48d3aa5ff35e5bd8a99c6255f832e4
nail00749 <71979155+nail00749@users.noreply.github.com>
2021-04-29 21:15:26 +0500

    Changes by super

--- src/Components/CardsMenu.js
+++ src/Components/CardsMenu.js
@@ -6,7 +6,15 @@ class CardsMenu extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            count: 2
+            count: 2,
+            images: ['https://sun9-59.userapi.com/impg/W0jlg8LiqlLMUNzgb_zUM6IGv6QEKZZj1Lb08g/yTN939Wnq34.jpg?size=403x343&quality=96&sign=42cc8c3f7ae969cf5e792d267e189bd2&type=album',
+            'https://sun9-52.userapi.com/impg/NgJISu3TJvuhDZvXbGe0mTM5rdgd1idmO02B1w/R-h1irLrCh0.jpg?size=978x1080&quality=96&sign=8433f9b209bf58485615f03013884afd&type=album',
+            'https://sun9-38.userapi.com/impg/Gag99FCPvSyOi-Xwgenp5aEm8uLeliIHUOzo1g/uLQS7OjVaZc.jpg?size=873x1080&quality=96&sign=025802739c5eae2052a538d0a42bb043&type=album',
+            'https://sun9-19.userapi.com/impg/NvdTnBRiWjMRrOaMdHg6Rnhb5c5OBHeyquwnOg/7njsEUIEVW4.jpg?size=800x800&quality=96&sign=0c51e31cc46c85a288e4e9a32e9c1b04&type=album',
+            'https://sun9-30.userapi.com/impg/mT5IZidLXgvaiDMSAHveDali3koIuvq-d90TPA/KPB_5Jiu2uY.jpg?size=700x750&quality=96&sign=a4f22851739a1ce88226e88212086d02&type=album',
+            'https://sun9-11.userapi.com/impg/cN9X8Q7uk_sPI4B-7zvtAb1jEoULWufFjjM0Iw/YtfmfRTNwyA.jpg?size=508x194&quality=96&sign=a4a690f502b3bc5b23eca980d8ebb6fa&type=album',
+            'https://sun9-54.userapi.com/impg/kUhXbpMXTkAeSsZzTQVKaD33nsKr3K2DwBnTHA/m-tihTDgXiU.jpg?size=900x650&quality=96&sign=6cbbb7250cbc4db8e7602dee51cc62fe&type=album',
+            'https://sun9-33.userapi.com/impg/CzVjtdmNngKJ5CQ8lMtHikS4G38dugsiu8mc7g/Td9r2IvGxEM.jpg?size=640x426&quality=96&sign=6b8f2fe37889d141aeaa8b71702bc1fd&type=album']
         }
     }
 
@@ -48,7 +56,7 @@ class CardsMenu extends React.Component {
             let listItems = listF.map((dataCards, i) =>
                 <div key={dataCards.id} className={i > this.state.count ? "hidden" : "Card"} onClick={this.props.click}>
                     <div className="CardImage ">
-                        <img src='https://smoking-shop.ru/upload/catalog_files/ce4/ce49cfe5904c8982d9aba7a61d699d4a.jpg'
+                        <img src={this.state.images[dataCards.id % 8]}
                              alt='hmmm'/>
                     </div>
                     <div className="Container">
